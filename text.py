78d83df87b5a60fc3c6b4a44fa2ca634be8a0b56
DUMUduwaka <mihirangausitha@gmail.com>
2022-07-01 07:11:58 +0530

    formatting

--- text.py
+++ text.py
@@ -4,11 +4,11 @@ python file
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=np.linspace(0,10, 100)
-y=x**3
+x = np.linspace(0, 10, 100)
+y = x**3
 
 plt.figure()
-plt.plot(x,y,'-')
+plt.plot(x, y, '-')
 plt.title('Y vs X')
 plt.xlabel('X')
 plt.ylabel('Y')

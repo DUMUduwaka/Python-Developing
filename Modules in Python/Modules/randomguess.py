150ec9bba80eff2969147b168919027d060d088b
DUMUduwaka <mihirangausitha@gmail.com>
2022-07-20 23:45:40 +0530

    A game

---
+++ Modules in Python/Modules/randomguess.py
@@ -0,0 +1,20 @@
+
+from random import randint
+num = randint(1,10)
+
+
+while True:
+    try:
+        guess = int(input('Guess the number! '))
+        if guess>0 and guess<11:
+            if num == guess:
+                print('Congratulations, You guessed the number.')
+                break
+            else:
+                print('Ooops You are wrong')
+        else:
+            print('Please enter a number between 1 and 10')
+    except ValueError:
+        print("Please enter a number")
+        continue
+

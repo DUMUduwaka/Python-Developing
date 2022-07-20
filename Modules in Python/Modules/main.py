18d1c889b46a1eb13281c63356bbbee8cd446707
DUMUduwaka <mihirangausitha@gmail.com>
2022-07-20 23:44:40 +0530

    Built in modules

--- Modules in Python/Modules/main.py
+++ Modules in Python/Modules/main.py
@@ -1,9 +1,22 @@
-import utility
-import Shopping.shopping_cart as sp
+# from utility import multiply,divide
+# from shopping import shopping_cart
+#
+# if __name__ == '__main__':
+#     print(shopping_cart.buy)
+#     print(multiply(3.5,12))
+#     print(divide(2,3))
+#     print(shopping_cart.buy('car'))
+#     print(shopping_cart.buy('van'))
+#
+# print(__name__)
+
+import random
+
+print(random)
+print(random.random())
+print(random.randint(0,10))
 
 
-print(utility.multiply(2, 3))
-print(sp)
-
-print(sp.buy('car'))
-print(sp.buy('van'))
+my_list = [1,2,3,4,5,6,7,8,9,10]
+random.shuffle(my_list)
+print(my_list)

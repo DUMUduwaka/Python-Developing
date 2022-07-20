8051931df3b877cf874835dda4861d396081ae7c
DUMUduwaka <mihirangausitha@gmail.com>
2022-07-20 12:34:09 +0530

    main file

--- Modules in Python/Modules/main.py
+++ Modules in Python/Modules/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import utility
+import Shopping.shopping_cart as sp
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(utility.multiply(2, 3))
+print(sp)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(sp.buy('car'))
+print(sp.buy('van'))
